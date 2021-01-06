3f3b4769dcc6b9f90a949299aa524e1ff59c6ecc
onifs10 <onifadebolu64@gmail.com>
2021-01-06 17:42:55 +0100

    google cloud added

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -8,10 +8,10 @@ class WebHookController extends Controller
 {
 
     function hook(Request  $request){
-         $data  =    $request->all();
+         $data  =  $request->all();
          $text = $data;
          $responseObj = new \stdClass();
-         $message = $this->createMessage(json_encode($text));
+         $message = $this->createMessage($text);
          $responseObj->fulfillmentMessages = [ $message ];
 
          return response()->json($responseObj,200 );
